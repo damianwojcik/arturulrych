e41f998ee250fb4601ebd97a766b85508e48ce95
Damian Wójcik <damian.wojcik@agora.pl>
2019-06-03 07:21:26 +0200

    change footer link

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
         <p class="copy">
             <span class="copy__line">Prawa autorskie &copy; 2017 <a href="<?= SITE_URL; ?>">arturulrych.pl</a></span> <span class="copy__divider">|</span>
             <span class="copy__line">Projekt: <a href="http://www.soniaulrych.pl" target="_blank">soniaulrych.pl</a></span>
-            <span class="copy__line"><span class="copy__divider">|</span> Web: <a href="http://www.damianwojcik.it" target="_blank">damianwojcik.it</a></span>
+            <span class="copy__line"><span class="copy__divider">|</span> Web: <a href="http://dwojcik.pro/" target="_blank">dwojcik.pro</a></span>
         </p>
 
     </div><!-- /.container -->
